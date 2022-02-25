cee3f0848db5a07279df71ec7cbf13f3dacc8422
Paulo Oliveira <paulo@ninito.me>
2022-02-25 16:48:06 -0300

    Comment out RecursionMath to avoid compilation problems

--- HackerRankPractice/HackerRankPractice/Other/Math/RecursionMath.cs
+++ HackerRankPractice/HackerRankPractice/Other/Math/RecursionMath.cs
@@ -1,27 +1,27 @@
-﻿using System;
-
-namespace HackerRankPractice.Other
-{
-	public class RecursionMath
-	{
-		public static int GetFactorialOf(int number)
-		{
-			return number switch
-			{
-				< 0 => throw new ArgumentOutOfRangeException(nameof(number)),
-				0 => 1,
-				_ => number * GetFactorialOf(number - 1)
-			};
-		}
-
-		public static int GetTermOfFibonacci(int term)
-		{
-			return term switch
-			{
-				< 0 => throw new ArgumentOutOfRangeException(nameof(term)),
-				< 3 => 1,
-				_ => GetTermOfFibonacci(term - 2) + GetTermOfFibonacci(term - 1)
-			};
-		}
-	}
-}
+﻿// using System;
+//
+// namespace HackerRankPractice.Other
+// {
+// 	public class RecursionMath
+// 	{
+// 		public static int GetFactorialOf(int number)
+// 		{
+// 			return number switch
+// 			{
+// 				< 0 => throw new ArgumentOutOfRangeException(nameof(number)),
+// 				0 => 1,
+// 				_ => number * GetFactorialOf(number - 1)
+// 			};
+// 		}
+//
+// 		public static int GetTermOfFibonacci(int term)
+// 		{
+// 			return term switch
+// 			{
+// 				< 0 => throw new ArgumentOutOfRangeException(nameof(term)),
+// 				< 3 => 1,
+// 				_ => GetTermOfFibonacci(term - 2) + GetTermOfFibonacci(term - 1)
+// 			};
+// 		}
+// 	}
+// }
